1e0c13dd7a926ba7760bb787d3ef8b571c7fe518
Indri Muska <indrimuska@gmail.com>
2015-08-26 14:25:20 +0200

    Add examples section

--- demo.js
+++ demo.js
@@ -1,2 +1,25 @@
 angular
 	.module('AngularSelectorDemo', ['selector'])
+	.controller('AngularSelectorDemoCtrl', ['$http', '$scope', function ($http, $scope) {
+		$scope.examples = [];
+		
+		$http.jsonp('examples.json')
+			.then(function (data) {
+				$scope.examples = data.data;
+			});
+	}])
+	.filter('highlight', ['$sce', function ($sce) {
+		return function (input, lang) {
+			return $sce.trustAsHtml(lang && input ? hljs.highlight(lang, input).value : input);
+		};
+	}])
+	.directive('example', ['$compile', function ($compile) {
+		return {
+			restrict: 'C',
+			link: function ($scope, $element) {
+				$element.html($scope.example.html);
+				$compile($element.contents())($scope);
+				eval($scope.example.js);
+			}
+		};
+	}])
