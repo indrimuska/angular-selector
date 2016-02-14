008f4acf9f204c41559f229292ab872a311a8db8
Indri Muska <indrimuska@gmail.com>
2016-02-14 13:43:52 +0100

    Added PLUNKER link to update examples

--- demo.js
+++ demo.js
@@ -23,3 +23,73 @@ angular
 			}
 		};
 	}])
+	.directive('plunker', [function () {
+		return {
+			restrict: 'A',
+			template: (
+					'<form method="post" action="http://plnkr.co/edit/?p=preview" target="_blank">' +
+						'<button type="submit">' +
+							'<img src="https://plnkr.co/img/plunker.png" alt="Plunker">&nbsp; Plunker' +
+						'</button>' +
+					'</form>'
+			),
+			link: function ($scope, $element, $attrs) {
+				var example = $scope.example;
+					form    = angular.element($element[0].querySelector('form')),
+					desc    = angular.element('<input type="hidden" name="description">').val('Angular Selector example: ' + example.title),
+					html    = angular.element('<input type="hidden" name="files[index.html]">'),
+					css     = angular.element('<input type="hidden" name="files[style.css]">'),
+					js      = angular.element('<input type="hidden" name="files[script.js]">');
+				
+				// plunker settings
+				form.append(angular.element('<input type="hidden" name="private" value="true">'));
+				['select', 'angular', 'selector', 'directive', 'typeahead', 'tag'].concat(example.title.toLowerCase().split(' ')).forEach(function (tag, index) {
+					var element = angular.element('<input type="hidden" name="tags[' + index + ']">').val(tag);
+					form.append(element);
+				});
+				form.append(desc);
+				
+				html.val([
+					'<!DOCTYPE html>',
+					'<html ng-app="myApp" ng-controller="ExampleCtrl as ctrl">',
+					'	<head>',
+					'		<meta charset="utf-8">',
+					'		<meta http-equiv="X-UA-Compatible" content="IE=edge">',
+					'		<meta name="viewport" content="width=device-width, initial-scale=1">',
+					'		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">',
+					'		<link rel="stylesheet" href="//cdn.rawgit.com/indrimuska/angular-selector/master/dist/angular-selector.css">',
+					'		<link rel="stylesheet" href="style.css">',
+					'	</head>',
+					'	<body>',
+					'		<h1 class="page-header">' + example.title + '</h1>',
+					'		',
+					'		' + example.html.replace(/\n/g, "\n\t\t"),
+					'		',
+					'		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.1/angular.js"></script>',
+					'		<script src="//cdn.rawgit.com/indrimuska/angular-selector/master/dist/angular-selector.js"></script>',
+					'		<script src="script.js"></script>',
+					'	</body>',
+					'</html>'
+				].join("\n"));
+				form.append(html);
+				
+				css.val([
+					'body { margin: 30px; }',
+					'p { margin-top: 10px; }',
+					'dl { margin-top: 15px; }',
+					'.btn-group { margin-bottom: 15px; }',
+					'.checkbox-inline { font-weight: bold; margin-bottom: 10px; }'
+				].join("\n"));
+				form.append(css);
+				
+				js.val([
+					'angular',
+					'	.module(\'myApp\', [\'selector\'])',
+					'	.controller(\'ExampleCtrl\', [\'$scope\', function ($scope) {',
+					'		' + (example.js || '').replace(/\n/g, "\n\t\t"),
+					'	}]);'
+				].join("\n"));
+				form.append(js);
+			}
+		};
+	}]);
