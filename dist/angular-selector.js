2dfdce31ddb4b01cb7040b1b418bd7ee193966d8
Indri Muska <indrimuska@gmail.com>
2016-04-21 23:01:09 +0200

    Prepping v1.3.8

--- dist/angular-selector.js
+++ dist/angular-selector.js
@@ -3,6 +3,8 @@
 	// Key codes
 	var KEYS = { up: 38, down: 40, left: 37, right: 39, escape: 27, enter: 13, backspace: 8, delete: 46, shift: 16, leftCmd: 91, rightCmd: 93, ctrl: 17, alt: 18, tab: 9 };
 	
+	var $filter, $timeout, $window, $http, $q;
+	
 	var Selector = (function () {
 		
 		function getStyles(element) {
